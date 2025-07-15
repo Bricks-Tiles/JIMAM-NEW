91caf5b7b52c48ac5fd4c560c0df7f65b0471f80
Akosua Anima <akosuaanima73@gmail.com>
2025-07-15 18:18:48 +0000

    worked in the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import Navbar from './component/Navbar'
 import Footer from './component/Footer'
 import { Contact } from './component/Contact'
+import { Homepage } from './component/Homepage'
 
 const App = () => {
   return (
     <div className="app-container">
       <Navbar />
+      <Homepage />
       <div className="main-content">
-        <h1>Welcome to Our Site</h1>
-        <p>This is some example text to show the main area.</p>
-        <p>Scroll down to see the footer at the bottom.</p>
+        
       </div>
       
       <Contact />
